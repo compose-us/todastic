ad9904e31083f38efbe622784adc175e7ded810c
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-05-21 02:58:41 +0200

    Fix first few tests
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/server/storage.js
+++ src/server/storage.js
@@ -4,6 +4,10 @@ const { readFile } = require("fs");
 async function load(filePath) {
   try {
     const file = await promisify(readFile)(filePath);
+    const todos = file.toString().split(/\n/).filter(line => !/^\s*$/.test(line));
+    if (todos.every(todo => /^\s*\[.\]/.test(todo))) {
+      return { todos };
+    }
     throw new Error(`Error reading file ${filePath} as todastic format.`);
   } catch (error) {
     if (error.code === "ENOENT") {

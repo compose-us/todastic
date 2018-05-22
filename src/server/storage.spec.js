18b88e06209703bb2431ba6d64ba767e78bd0a98
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-05-22 19:54:42 +0200

    Add more examples and check they can be loaded
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/server/storage.spec.js
+++ src/server/storage.spec.js
@@ -1,28 +1,41 @@
 const storage = require("./storage");
 
 describe("Storage", () => {
-  it("will throw an error if file is missing", async () => {
-    const fp = `${__dirname}/__test__/missing.todo`;
-    expect.assertions(1);
-    await expect(storage.load(fp)).rejects.toThrow(/missing/i);
-  });
+  describe("loading of files", () => {
+    it("will throw an error if file is missing", async () => {
+      const fp = `${__dirname}/__test__/missing.todo`;
+      expect.assertions(1);
+      await expect(storage.load(fp)).rejects.toThrow(/missing/i);
+    });
 
-  it("will throw an error if file is in a wrong format", async () => {
-    const fp = `${__dirname}/__test__/wrong-format.todo`;
-    expect.assertions(1);
-    await expect(storage.load(fp)).rejects.toThrow(/format/i);
-  });
+    it("will throw an error if file is in a wrong format", async () => {
+      const fp = `${__dirname}/__test__/wrong-format.todo`;
+      expect.assertions(1);
+      await expect(storage.load(fp)).rejects.toThrow(/format/i);
+    });
 
-  it("will not throw errors if file is empty", async () => {
-    const fp = `${__dirname}/__test__/empty.todo`;
-    const store = await storage.load(fp);
-    expect(store.todos.length).toBe(0);
-  });
+    it("will not throw errors if file is empty", async () => {
+      const fp = `${__dirname}/__test__/empty.todo`;
+      const store = await storage.load(fp);
+      expect(store.todos.length).toBe(0);
+    });
 
-  it("can load a simple file without errors", async () => {
-    const fp = `${__dirname}/__test__/sample.todo`;
-    const store = await storage.load(fp);
-    expect(store.todos.length).toBeGreaterThan(0);
-  });
+    it("can load a simple file without errors", async () => {
+      const fp = `${__dirname}/__test__/simple-sample.todo`;
+      const store = await storage.load(fp);
+      expect(store.todos.length).toBeGreaterThan(0);
+    });
 
+    it("can load a file with subtasks without errors", async () => {
+      const fp = `${__dirname}/__test__/dependent-sample.todo`;
+      const store = await storage.load(fp);
+      expect(store.todos.length).toBeGreaterThan(0);
+    });
+
+    it("can load a file with a lot of subtasks without errors", async () => {
+      const fp = `${__dirname}/__test__/nested-sample.todo`;
+      const store = await storage.load(fp);
+      expect(store.todos.length).toBeGreaterThan(0);
+    });
+  });
 });

8777b129280375f8e9d133cc542010526e9ba61a
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-05-23 19:40:42 +0200

    Add a test for another wrong format
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/server/storage.spec.js
+++ src/server/storage.spec.js
@@ -14,11 +14,17 @@ describe("Storage", () => {
       await expect(storage.load(fp)).rejects.toThrow(/format/i);
     });
 
+    it("will throw an error if the subtasks are wrongly nested", async () => {
+      const fp = `${__dirname}/__test__/wrong-format-nested.todo`;
+      expect.assertions(1);
+      await expect(storage.load(fp)).rejects.toThrow(/format/i);
+    });
+
     it("will not throw errors if file is empty", async () => {
-      const fp = `${__dirname}/__test__/empty.todo`;
+      const fp = `${__dirname}/__test__/empty.todo`;  
       const store = await storage.load(fp);
       expect(store.todos.length).toBe(0);
-    });
+    });  
 
     it("can load a simple file without errors", async () => {
       const fp = `${__dirname}/__test__/simple-sample.todo`;

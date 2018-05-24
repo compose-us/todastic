f243dde86d731186a741e5af4f7a170adda41032
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-05-24 15:25:49 +0200

    Make sure the format of todos is always okay
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/server/storage.js
+++ src/server/storage.js
@@ -4,11 +4,29 @@ const { readFile } = require("fs");
 async function load(filePath) {
   try {
     const file = await promisify(readFile)(filePath);
-    const todos = file.toString().split(/\n/).filter(line => !/^\s*$/.test(line));
-    if (todos.every(todo => /^\s*\[.\]/.test(todo))) {
-      return { todos };
-    }
-    throw new Error(`Error reading file ${filePath} as todastic format.`);
+    const todos = file.toString().split(/\n/);
+    const result = todos.reduce(
+      (res, line) => {
+        const matches = /^(\s*\[.\])/.exec(line);
+        if (!matches) {
+          if (/^\s*$/.test(line)) {
+            return res;
+          } else {
+            throw new Error(`Error reading file ${filePath} as todastic format.`);
+          }
+        }
+        if (matches[1].length > res.lastLine + 2) {
+          throw new Error(`Error reading file ${filePath} as todastic format.`);
+        }
+        res.lastLine = matches[1].length;
+        res.todos.push(line);
+        return res;
+      },
+      { todos: [], lastLine: '[ ]'.length }
+    );
+
+    const { lastLine, ...rest } = result;
+    return rest;
   } catch (error) {
     if (error.code === "ENOENT") {
       throw new Error(`File ${filePath} is missing or can not be found.`);

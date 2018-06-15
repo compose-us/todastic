e051d2d0190f220d5e75bcd4f3ae3d3449f290de
Udo Groebner <udo@groebnerweb.de>
2018-06-15 22:52:58 +0200

    Introduce socket.io to the webserver
    Signed-off-by: Udo Groebner <udo@groebnerweb.de>

--- packages/server-web/src/server.js
+++ packages/server-web/src/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 var server = express();
+var http = require('http').Server(server);
+var io = require('socket.io')(http);
 
 server.get('/', function (req, res) {
   res.sendFile(__dirname + '/static/index.html');
 });
 
-server.listen(3000, function () {
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function () {
   console.log('Todastic webserver listening on port 3000, Sire!');
 });
 

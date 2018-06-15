36a82ca65dd061e413c4e159af5cdb2dadfbf24b
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-06-15 21:09:45 +0200

    Add first REMOVED_TODO event tests
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- packages/storage-events/src/storage.js
+++ packages/storage-events/src/storage.js
@@ -1,13 +1,24 @@
 function replay(events) {
+  const addedTodoEvents = events.filter(event => event.event === "ADDED_TODO");
+  const removedTodoEvents = events.filter(event => event.event === "REMOVED_TODO");
+  const todos = removeTodos(addTodos(addedTodoEvents), removedTodoEvents);
+  return { todos };
+}
+
+function addTodos(events) {
   const parents = events.map(event => event.data).filter(event => event.parentId === undefined);
   const children = events.map(event => event.data).filter(event => event.parentId !== undefined);
   const todos = parents;
   children.forEach(todo => {
-    const foundParent = parents.find(parent => parent.id === todo.parentId);
+    const foundParent = todos.find(parent => parent.id === todo.parentId);
     foundParent.children = foundParent.children || [];
     foundParent.children.push(todo);
   });
-  return { todos };
+  return todos;
+}
+
+function removeTodos(todos, events) {
+  return todos.filter(todo => events.every(event => event.data.id !== todo.id));
 }
 
 module.exports = {

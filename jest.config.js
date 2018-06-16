d86afff7c7a6805deb68d82806c9f71ad52ca624
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-06-16 21:29:36 +0200

    Ignore some JS in coverage as long as Jest is not supporting ES modules
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ["packages/**/*.{js}", "!**/node_modules/**"],
+  collectCoverageFrom: [
+    "packages/**/*.{js}",
+    "!**/node_modules/**",
+    "!**/*.config.js",
+    "!**/server-web/src/client/**/*.js"
+  ],
   roots: ["packages/", "tests/"]
 };

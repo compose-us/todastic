1f96b0cf923aa0c0ba2787538fcbe96116ee612e
Udo Groebner <udo@groebnerweb.de>
2018-07-30 18:17:51 +0200

    Add winston logger

---
+++ packages/server-web/config/winston.js
@@ -0,0 +1,33 @@
+const winston = require('winston');
+var options = {
+ /* file: {
+    level: 'info',
+    filename: `${appRoot}/logs/app.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: false,
+  },*/
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    format: winston.format.simple(),
+    colorize: true,
+  },
+};
+var logger = winston.createLogger({
+  transports: [
+//    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console)
+  ],
+  exitOnError: false, // do not exit on handled exceptions
+});
+logger.stream = {
+  write: function(message, encoding) {
+    logger.info(message);
+  },
+};
+
+module.exports = logger;
--- packages/server-web/src/server.js
+++ packages/server-web/src/server.js
@@ -2,17 +2,33 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const createSocketOnServer = require("@todastic/server-socket");
+const logger = require('../config/winston');
+const morgan = require('morgan');
 
-app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+app.use(morgan('combined', {
+    stream: logger.stream
+}));
+app.get("/login", (req, res) => {
+    res.sendFile(`${__dirname}/login.html`);
 });
-app.get("/main.css", (req, res) => {
-  res.sendFile(`${__dirname}/main.css`);
-});
-app.use(express.static(`${__dirname}/dist/`));
+
+app.get("/",
+    (req, res) => {
+        res.sendFile(`${__dirname}/index.html`);
+    }
+);
+
+app.get("/main.css",
+    (req, res) => {
+        res.sendFile(`${__dirname}/main.css`);
+    }
+);
+app.use(
+    express.static(`${__dirname}/dist/`)
+);
 
 createSocketOnServer(server);
 
 server.listen(3000, function() {
-  console.log("Todastic webserver listening on port 3000, Sire!");
+    logger.info("Todastic webserver listening on port 3000, Sire!");
 });

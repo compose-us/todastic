73ec9644264274f0153e23927ca0fa5632a6b4ba
Udo Groebner <udo@groebnerweb.de>
2018-08-28 21:33:41 +0200

    Add proper shutdown hooks

--- packages/server-web/src/start.js
+++ packages/server-web/src/start.js
@@ -13,14 +13,35 @@ start()
   .catch(err => {
     logger.error(err);
     logger.error(err.stack);
-  })
-  .then(() => logger.end());
+  });
 
 async function start() {
   logger.info("Starting to set up server");
   const database = await initDatabase({ config, logger });
   logger.info("Database connection set up");
   const User = createUserModel({ mongoose: database.mongoose });
-  await startServer({ config, database, User, logger });
+  const httpServer = await startServer({ config, database, User, Event, logger });
+  registerShutdownHooks({ logger, httpServer, database });
+
   logger.info("Server started");
+}
+
+function registerShutdownHooks({ logger, httpServer, database }) {
+  registerShutdownHook({ signal: "SIGTERM", logger, httpServer, database });
+  registerShutdownHook({ signal: "SIGINT", logger, httpServer, database });
+}
+
+function registerShutdownHook({ signal, logger, httpServer, database }) {
+  process.on(signal, () => {
+    logger.info("Initiating shutdown sequence");
+    logger.info("Shutting down webserver");
+    httpServer.close();
+    logger.info("Shutting down database connection");
+    database.mongoose.disconnect().then(() => {
+      logger.info("Thank you for flying with Air Todastic today. Have a nice day.");
+      logger.info("Good bye.");
+      logger.end();
+      process.exit(0);
+    });
+  });
 }

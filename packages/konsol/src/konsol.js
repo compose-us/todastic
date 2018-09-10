7a7432efb3ba633eab765c314b232f9a1b126c43
Udo Groebner <udo.groebner@compose.us>
2018-09-10 10:36:08 -0300

    Add events to konsol

--- packages/konsol/src/konsol.js
+++ packages/konsol/src/konsol.js
@@ -2,10 +2,12 @@ const repl = require("repl");
 const config = require("@todastic/config");
 const { initDatabase } = require("@todastic/server-web/src/database-mongo.js");
 const { createUserModel } = require("@todastic/storage-users");
+const { createEventModel } = require("@todastic/storage-events");
 
 initDatabase({ config, logger: console }).then(db => {
   const myRepl = repl.start("> ");
   const ctx = myRepl.context;
   myRepl.context.User = createUserModel({ mongoose: db.mongoose });
+  myRepl.context.Event = createEventModel({ mongoose: db.mongoose });
   myRepl.context.db = db;
 });

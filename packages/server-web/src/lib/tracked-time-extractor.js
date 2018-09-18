63301741f1675ac830afe217c3d193b13dee20f7
Udo Groebner <udo.groebner@compose.us>
2018-09-18 16:55:47 -0300

    regex.exec preserves capture groups

--- packages/server-web/src/lib/tracked-time-extractor.js
+++ packages/server-web/src/lib/tracked-time-extractor.js
@@ -2,19 +2,15 @@ module.exports = { extractTrackedTimes };
 
 function extractTrackedTimes(inputString) {
   const regex = /#TRACK\((\{.*?\:.*?\})\)/gi;
-  const matches = inputString.match(regex);
+
   let text = inputString;
   let trackedTimes = [];
-  if (matches) {
-    trackedTimes = matches.map(match => {
-      text = text.replace(match, "");
-      // in theory, this should be provided as capture group by the regex engine.
-      // not sure why it doesn't work
-      const matchGroup = match.replace("#TRACK(", "").replace(/\)$/, "");
-      return JSON.parse(matchGroup);
-    });
-    text = text.replace(/\s+/g, " ").trim();
+  let match;
+  while ((match = regex.exec(inputString)) !== null) {
+    text = text.replace(match[0], "");
+    trackedTimes.push(JSON.parse(match[1]));
   }
+  text = text.replace(/\s+/g, " ").trim();
 
   return { trackedTimes, text };
 }

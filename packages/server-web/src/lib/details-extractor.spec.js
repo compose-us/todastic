0c3db5b994d98dfdad97a350d6ae2d02a0c858a5
Udo Groebner <udo.groebner@compose.us>
2018-09-18 16:45:12 -0300

    Make tests use toMatchSnapshot

--- packages/server-web/src/lib/details-extractor.spec.js
+++ packages/server-web/src/lib/details-extractor.spec.js
@@ -1,21 +1,11 @@
 const { extractDetails } = require("./details-extractor");
 
 describe("DetailsExtractor", () => {
-  it(
-    "extracts labels and tracked times",
-    testExtraction(
-      'wait for this nice #label do you? #TRACK({"trackedTime": "00:20:00", "tracker": "Steve", "date": "2018-09-18 14:06:00"})',
-      ["#label"],
-      [{ trackedTime: "00:20:00", tracker: "Steve", date: "2018-09-18 14:06:00" }],
-      "wait for this nice label do you?"
-    )
-  );
+  it("extracts labels and tracked times", () => {
+    expect(
+      extractDetails(
+        'wait for this nice #label do you? #TRACK({"trackedTime": "00:20:00", "tracker": "Steve", "date": "2018-09-18 14:06:00"})'
+      )
+    ).toMatchSnapshot();
+  });
 });
-function testExtraction(inputString, expectedLabels, expectedTrackedTimes, expectedResultString) {
-  return () => {
-    const result = extractDetails(inputString);
-    expect(result.labels).toEqual(expectedLabels);
-    expect(result.trackedTimes).toEqual(expectedTrackedTimes);
-    expect(result.text).toBe(expectedResultString);
-  };
-}
--- packages/server-web/src/lib/tracked-time-extractor.spec.js
+++ packages/server-web/src/lib/tracked-time-extractor.spec.js
@@ -1,35 +1,21 @@
 const { extractTrackedTimes } = require("./tracked-time-extractor");
 
 describe("TrackedTimeExtractor", () => {
-  it(
-    "doesn't change anything when no tracked time is in the string",
-    testExtraction("no time #really", [], "no time #really")
-  );
-  it(
-    "can parse a simple example",
-    testExtraction(
-      'worked on #TRACK({"trackedTime": "01:00:00", "tracker": "Jörn", "date": "2018-09-13 17:27:00"})',
-      [{ trackedTime: "01:00:00", tracker: "Jörn", date: "2018-09-13 17:27:00" }],
-      "worked on"
-    )
-  );
-  it(
-    "can parse more than one entry",
-    testExtraction(
-      'worked on #TRACK({"trackedTime": "00:10:00", "tracker": "Udo", "date": "2018-09-15 18:20:15"}) #TRACK({"trackedTime": "01:00:00", "tracker": "Jörn", "date": "2018-09-13 17:27:00"}) #great',
-      [
-        { trackedTime: "00:10:00", tracker: "Udo", date: "2018-09-15 18:20:15" },
-        { trackedTime: "01:00:00", tracker: "Jörn", date: "2018-09-13 17:27:00" }
-      ],
-      "worked on #great"
-    )
-  );
+  it("doesn't change anything when no tracked time is in the string", () => {
+    expect(extractTrackedTimes("no time #really")).toMatchSnapshot();
+  });
+  it("can parse a simple example", () => {
+    expect(
+      extractTrackedTimes(
+        'worked on #TRACK({"trackedTime": "01:00:00", "tracker": "Jörn", "date": "2018-09-13 17:27:00"})'
+      )
+    ).toMatchSnapshot();
+  });
+  it("can parse more than one entry", () => {
+    expect(
+      extractTrackedTimes(
+        'worked on #TRACK({"trackedTime": "00:10:00", "tracker": "Udo", "date": "2018-09-15 18:20:15"}) #TRACK({"trackedTime": "01:00:00", "tracker": "Jörn", "date": "2018-09-13 17:27:00"}) #great'
+      )
+    ).toMatchSnapshot();
+  });
 });
-
-function testExtraction(inputString, expectedEntries, expectedResultString) {
-  return () => {
-    const result = extractTrackedTimes(inputString);
-    expect(result.trackedTimes).toEqual(expectedEntries);
-    expect(result.text).toBe(expectedResultString);
-  };
-}

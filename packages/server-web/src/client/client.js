bf7a7f16d83246c61a3e99e6b7e9c81c56691567
Udo Groebner <udo.groebner@compose.us>
2018-09-24 09:56:50 -0300

    remove dead code

--- packages/server-web/src/client/client.js
+++ packages/server-web/src/client/client.js
@@ -1,6 +1,5 @@
 import createSocketConnection from "./socket-connection.js";
 import createApp from "./create-app.js";
-//import { processEvent } from "./todo-store.js";
 import { store } from "./store.js";
 
 const processEvent = event => store.commit("processEvent", event);

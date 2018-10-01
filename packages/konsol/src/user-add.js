bc93a65302c3f5d98cfe8c04d7692358174cea70
Udo Groebner <udo.groebner@compose.us>
2018-10-01 12:14:41 -0300

    Add userId to events. Extract labels. Autogenerate password.

--- packages/konsol/src/user-add.js
+++ packages/konsol/src/user-add.js
@@ -13,13 +13,18 @@ yargs
       return yargs.option("from").array("status");
     },
     createUser
-  )
+  ).argv
 
 async function createUser(argv) {
   const username = argv.u;
-  const password = argv.p;
-  if (username === undefined || password === undefined) {
-    throw "Need username and password."
+  let password = argv.p;
+  if (username === undefined) {
+    throw "Need username"
+  }
+  if (password === undefined) {
+    console.log("No password given, generating one.");
+    password = Math.random().toString(36).slice(-8);
+    console.log("Using password '" + password + "'");
   }
   const db = await initDatabase({ config, logger: console })
   const User = createUserModel({ mongoose: db.mongoose });
@@ -27,24 +32,24 @@ async function createUser(argv) {
   const user = await User.create({ username, password })
   const uid = user._id + "-id-";
   const events = [
-    { eventType: "ADDED_TODO", data: { todoId: uid + "1", title: "Ein erstes Todo anlegen", position: 0 } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "1-1", title: "Ein erstes SUB-Todo anlegen #power", position: 0, parentId: uid + "1" } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "1-1-1", title: "Auf die drei Punkte klicken", position: 0, parentId: uid + "1-1" } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "1-1-2", title: "\"plus\" anklicken", position: 1, parentId: uid + "1-1" } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "1-1-3", title: "In das Textfeld ein neues Sub-Todo schreiben", position: 2, parentId: uid + "1-1" } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "1-2", title: "Ein Todo an eine andere Stelle verschieben", position: 1, parentId: uid + "1" } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "1-2-1", title: "Drag und drop verwenden", position: 0, parentId: uid + "2-1" } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "2", title: "Passwort ändern #security", position: 1 } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "3", title: "Dem neugierigen Todastic Team tiefgründiges Feedback geben #help-us", position: 2 } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "3-1", title: "Über das Fragezeichen (?) rechts oben die Feedback E-Mailadresse finden", position: 0, parentId: uid + "3" } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "3-2", title: "2 Minuten mit Todastic herumspielen #fun", position: 1, parentId: uid + "3" } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "4", title: "Eine Zeit tracken", position: 3 } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "4-1", title: "Zeit eintragen", position: 0, parentId: uid + "4" } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "4-2", title: "Zeit ändern", position: 1, parentId: uid + "4" } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "4-3", title: "Die getrackte Zeit über ein Skript ausgeben lassen", position: 2, parentId: uid + "4" } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "4-3-1", title: "Skript aus dem Wiki kopieren (https://github.com/compose-us/todastic/wiki/User-scripts)", position: 0, parentId: uid + "4-3" } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "4-3-2", title: "Skript laufen lassen", position: 1, parentId: uid + "4-3" } },
-    { eventType: "ADDED_TODO", data: { todoId: uid + "4-3-3", title: "Ergebnis begutachten #yay", position: 2, parentId: uid + "4-3" } }
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "1", title: "Ein erstes Todo anlegen", position: 0 } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "1-1", labels: ["power"], title: "Ein erstes SUB-Todo anlegen", position: 0, parentId: uid + "1" } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "1-1-1", title: "Auf die drei Punkte klicken", position: 0, parentId: uid + "1-1" } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "1-1-2", title: "\"plus\" anklicken", position: 1, parentId: uid + "1-1" } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "1-1-3", title: "In das Textfeld ein neues Sub-Todo schreiben", position: 2, parentId: uid + "1-1" } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "1-2", title: "Ein Todo an eine andere Stelle verschieben", position: 1, parentId: uid + "1" } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "1-2-1", title: "Drag und drop verwenden", position: 0, parentId: uid + "2-1" } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "2", labels: ["security"], title: "Passwort ändern", position: 1 } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "3", labels: ["help-us"], title: "Dem neugierigen Todastic Team tiefgründiges Feedback geben", position: 2 } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "3-1", title: "Über das Fragezeichen (?) rechts oben die Feedback E-Mailadresse finden", position: 0, parentId: uid + "3" } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "3-2", labels:["#fun"], title: "2 Minuten mit Todastic herumspielen #fun", position: 1, parentId: uid + "3" } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "4", title: "Eine Zeit tracken", position: 3 } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "4-1", title: "Zeit eintragen", position: 0, parentId: uid + "4" } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "4-2", title: "Zeit ändern", position: 1, parentId: uid + "4" } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "4-3", title: "Die getrackte Zeit über ein Skript ausgeben lassen", position: 2, parentId: uid + "4" } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "4-3-1", title: "Skript aus dem Wiki kopieren (https://github.com/compose-us/todastic/wiki/User-scripts)", position: 0, parentId: uid + "4-3" } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "4-3-2", title: "Skript laufen lassen", position: 1, parentId: uid + "4-3" } },
+    { eventType: "ADDED_TODO", userId: user._id, data: { todoId: uid + "4-3-3", labels: ["#yay"], title: "Ergebnis begutachten", position: 2, parentId: uid + "4-3" } }
   ];
   events.forEach(event => { Event.create(event).then(e => console.log(e)) });
 }

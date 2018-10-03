674047617cf851d3774efd63dc6be4a0d111995a
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-10-04 00:12:41 +0200

    Fix babel 7, add storybook to global scripts
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- packages/server-web/babel.config.js
+++ packages/server-web/babel.config.js
@@ -1,21 +1,21 @@
-{
-  "presets": [
+module.exports = {
+  presets: [
     [
       "@babel/preset-env",
       {
-        "targets": {
-          "browsers": ["last 2 versions"]
+        targets: {
+          browsers: ["last 2 versions"]
         },
-        "useBuiltIns": "entry"
+        useBuiltIns: "entry"
       },
       "vue"
     ]
   ],
-  "plugins": [
+  plugins: [
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-proposal-object-rest-spread",
     "@babel/plugin-proposal-unicode-property-regex",
     "@babel/plugin-syntax-dynamic-import"
   ],
-  "retainLines": true
-}
+  retainLines: true
+};

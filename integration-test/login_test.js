813fbcbfc94749ec23c70f925927361cd563151d
Udo Groebner <udo.groebner@compose.us>
2018-10-19 17:54:15 -0300

    Add user login e2e test

--- integration-test/login_test.js
+++ integration-test/login_test.js
@@ -4,3 +4,11 @@ Scenario("there is a login form", I => {
   I.amOnPage("http://localhost:3000");
   I.see("Login");
 });
+
+Scenario("I can actually log in", I => {
+  I.amOnPage("http://localhost:3000");
+  I.fillField("Username", "John");
+  I.fillField("Password", "Snow");
+  I.click("Login");
+  I.see("Logout");
+});

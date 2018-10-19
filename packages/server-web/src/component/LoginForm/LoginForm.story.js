668071a7f21fa093cd895f91d6f3264342fe3be5
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-10-20 01:54:39 +0200

    Style LoginForm
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- packages/server-web/src/component/LoginForm/LoginForm.story.js
+++ packages/server-web/src/component/LoginForm/LoginForm.story.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 
 import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
 import LoginForm from "./LoginForm.vue";
 Vue.component("LoginForm", LoginForm);
 
@@ -19,7 +20,9 @@ storiesOf("LoginForm", module)
   .add("succeeding", () => ({
     components: { LoginForm },
     render() {
-      const login = async () => {};
+      const login = async (...args) => {
+        action("logging in")(...args);
+      };
       return <LoginForm verifyLogin={login} />;
     }
   }));
--- packages/server-web/src/component/LoginForm/LoginForm.vue
+++ packages/server-web/src/component/LoginForm/LoginForm.vue
@@ -1,12 +1,16 @@
 <template>
   <div :class="$style.root">
-    <todastic-logo />
-    <form v-on:submit.prevent="login()">
-      <input type="text" name="username" v-model="input.username" placeholder="Username" />
-      <input type="password" name="password" v-model="input.password" placeholder="Password" />
-      <div v-if="this.errorMessage" :class="$style.errorInfo">{{this.errorMessage}}</div>
-      <button type="submit">Login</button>
-    </form>
+    <div :class="$style.wrapper">
+      <todastic-logo :class="$style.logo" />
+      <form v-on:submit.prevent="login()">
+        <div :class="$style.formWrapper">
+          <input type="text" name="username" v-model="input.username" placeholder="Username" />
+          <button :class="$style.loginButton" type="submit">Login</button>
+          <input type="password" name="password" v-model="input.password" placeholder="Password" />
+          <div :class="$style.errorInfo" v-if="this.errorMessage">{{this.errorMessage}}</div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -29,7 +33,7 @@ export default {
   methods: {
     async login() {
       try {
-        await this.verifyLogin(this.username, this.password);
+        await this.verifyLogin(this.input.username, this.input.password);
         this.errorMessage = null;
       } catch (err) {
         this.errorMessage = err.message;
@@ -40,19 +44,58 @@ export default {
 </script>
 
 <style lang="scss" module>
+$max-box-size: 450px;
+$min-box-size: 320px;
+
 .root {
   display: flex;
-  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   height: 100%;
   width: 100%;
-  border: 1px solid red;
 }
 
 // elements
 
+.wrapper {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  min-height: $min-box-size;
+  min-width: $min-box-size;
+  width: 95%;
+  max-height: $max-box-size;
+  max-width: $max-box-size;
+  padding: 25px;
+  box-shadow: 0 0 1em $color-font;
+}
+
+.logo {
+  align-self: center;
+  margin-bottom: 25px;
+}
+
+.formWrapper {
+  display: grid;
+  grid-template-columns: 1fr max-content;
+}
+
+.loginButton {
+  grid-row: span 2;
+}
+
 .errorInfo {
-  color: $error-text;
-  padding: 2em 0;
+  position: absolute;
+  color: $color-notification;
+  border-left: 5px solid $color-notification;
+  background-color: rgba($color-notification, 0.1);
+  text-align: left;
+  padding: 5px 5px;
+  bottom: 25px;
+  left: 25px;
+  right: 25px;
 }
 
 input {
@@ -60,7 +103,7 @@ input {
   border: 0;
   box-shadow: 0 2px 1px -1px #000;
   padding: 10px;
-  width: 30%;
+  width: 100%;
   margin-right: 0.5%;
   background-color: rgba(0, 0, 0, 0.05);
 }

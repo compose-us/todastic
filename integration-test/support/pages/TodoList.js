98b6835d40743462c8eda507b870817750e98157
Udo Groebner <udo.groebner@compose.us>
2018-10-24 18:18:04 -0300

    some more element selectors

--- integration-test/support/pages/TodoList.js
+++ integration-test/support/pages/TodoList.js
@@ -5,19 +5,27 @@ module.exports = {
   newFormEl: () =>
     'div.src-client-component-Home__todos > div.src-client-component-TodoText__todoText > input[type="text"]',
   listEl: () => ".src-client-component-TodoList__todoList > ul",
+  optionsMenuEl: () => ".src-client-component-TodoOptions__options",
+  destroyEl: () => ".src-client-component-TodoOptions__remove",
+  addSubtodoEl: () => ".src-client-component-TodoOptions__add",
+  todoItemEl: () => ".src-client-component-TodoItem__todo",
+  todoItemTitleEl: () => ".src-client-component-TodoItem__title",
 
   todoEl(position) {
+    console.log(
+      "FIXME: getting a Todo at a specific position doesn't work under all circumstances. Use this step with CAUTION"
+    );
     return `.src-client-component-TodoList__todoList > ul > li:nth-child(${position})`;
   },
 
   todoDestroyEl(position) {
-    return locate(".src-client-component-TodoOptions__remove").inside(locate(`${this.todoEl(position)}`));
+    return locate(this.todoEl(position)).find(locate(this.destroyEl()));
   },
   todoAddSubTodoEl(position) {
-    return locate(".src-client-component-TodoOptions__add").inside(locate(`${this.todoEl(position)}`));
+    return locate(this.addSubtodoEl()).inside(locate(`${this.todoEl(position)}`));
   },
   todoOptionsMenuEl(position) {
-    return locate(".src-client-component-TodoOptions__options").inside(locate(`${this.todoEl(position)}`));
+    return locate(this.optionsMenuEl()).inside(locate(`${this.todoEl(position)}`));
   },
 
   // Interactions
@@ -40,6 +48,13 @@ module.exports = {
   },
 
   remove(position) {
+    this.toggleOptionsMenu(position);
     I.click(this.todoDestroyEl(position));
+  },
+
+  removeLastTodo() {
+    const lastTodo = locate("#ul li").last();
+    I.click(locate(this.optionsMenuEl()).inside(lastTodo));
+    I.click(locate(this.destroyEl()).inside(lastTodo));
   }
 };
